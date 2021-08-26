70cb72ff166b6c6201821260738133ff80079caa
TommyTran1 <tt01688@srcs.org>
2021-08-26 15:09:33 -0700

    Crab tutorial 2

--- Crab.java
+++ Crab.java
@@ -7,6 +7,27 @@ public class Crab extends Actor
 {
     public void act()
     {
+         move(-50);
+         turnAtEdge();
+         checkKeyPress();
+         onCollision();   
+    }
+    
+    public void turnAtEdge()
+    {
+        if(isAtEdge())
+        {
+            turn(50);
+        }
+    }
+    
+    public void checkKeyPress()
+    {
+        
+    }
+    
+    public void onCollision()
+    {
         
     }
 }
